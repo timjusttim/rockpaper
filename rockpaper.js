883ff1fa173c6aea2d093f030495d7118d983e93
Tim Song <timothyd.song@gmail.com>
2023-11-14 16:39:21 -1000

    Fixed comparison operators

--- rockpaper.js
+++ rockpaper.js
@@ -1,43 +1,33 @@
-function rockpaperscissor(playerchoice, computerchoice) {
-    playerchoice = playerchoice.toLowerCase()
-    computerchoice = computerchoice.toLowerCase()
+function rockpaperscissor(player, computer) {
+    playerchoice = player.toLowerCase()
+    computerchoice = computer.toLowerCase()
     
-    if (playerchoice = "rock") {
-        if (computerchoice = "rock") {
+    if (playerchoice === "rock")
+        if (computerchoice === "rock")
             return "It's a tie"
-        }
-        else if (computerchoice = "paper") {
+        else if (computerchoice === "paper")
             return "The computer wins!"
-        }
-        else if (computerchoice = "scissors") {
+        else if (computerchoice === "scissors")
             return "The player wins!"
-        }
-    }
-    else if (playerchoice = "paper") {
-        if (computerchoice = "rock") {
+
+    else if (playerchoice === "paper")
+        if (computerchoice === "rock")
             return "The player wins!"
-        }
-        else if (computerchoice = "paper") {
+        else if (computerchoice === "paper")
             return "It's a tie!"
-        }
-        else if (computerchoice = "scissors") {
+        else if (computerchoice === "scissors")
             return "The computer wins!"
-        }
-    }
-    else if (playerchoice = "scissors") {
-        if (computerchoice = "rock") {
+    
+    else if (playerchoice === "scissors")
+        if (computerchoice === "rock")
             return "The computer wins!"
-        }
-        else if (computerchoice = "paper") {
+        else if (computerchoice === "paper")
             return "The player wins!"
-        }
-        else if (computerchoice = "scissors") {
+        else if (computerchoice === "scissors")
             return "It's a tie!"
-        }
-    }
 }
 
-playerchoice = "RoCk";
-computerchoice = "PaPeR";
+pc = "RoCk";
+cc = "PaPeR";
 
-console.log(rockpaperscissor(playerchoice, computerchoice));
+console.log(rockpaperscissor(pc, cc));
